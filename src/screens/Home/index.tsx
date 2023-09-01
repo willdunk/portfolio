878d7dbc93ccfc188e3ff5b0b9b3e8fa35cea090
William Dunkerley <will.eric.dunkerley@gmail.com>
2023-08-31 23:13:23 -0400

    Adding build

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Working on this. Just switching this over to a new</div>;
+  return <div>Working on this. Just switching this over to Typescript</div>;
 };
 
 export default Home;
