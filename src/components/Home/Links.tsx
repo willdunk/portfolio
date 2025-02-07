d47dc726a59f961dec885874a733df3cb8e3799e
William Dunkerley <will.eric.dunkerley@gmail.com>
2025-02-07 18:56:06 -0500

    Remove Twitter link

--- src/components/Home/Links.tsx
+++ src/components/Home/Links.tsx
@@ -1,13 +1,12 @@
 import { HStack, Icon, Link } from '@chakra-ui/react';
 import React from 'react';
-import { IoLogoTwitter, IoLogoLinkedin, IoLogoGithub } from 'react-icons/io';
+import { IoLogoLinkedin, IoLogoGithub } from 'react-icons/io';
 import { TbBrandLetterboxd } from 'react-icons/tb';
 
 const Links: React.FC<{}> = () => {
   const links = [
     { link: "https://www.linkedin.com/in/william-eric-dunkerley/", logo: IoLogoLinkedin, alt: "LinkedIn" },
     { link: "https://github.com/willdunk", logo: IoLogoGithub, alt: "GitHub" },
-    { link: "https://twitter.com/JGGhahaveryfun", logo: IoLogoTwitter, alt: "Twitter" },
     { link: "https://letterboxd.com/hahaveryfun/", logo: TbBrandLetterboxd, alt: "Letterboxd" }
   ];
   return (
